8b29e58c47a0edaf2b764229401ad26de35b3429
zhangbaihu <892471717@qq.com>
2019-08-03 17:25:32 +0800

    添加endless

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/fvbock/endless"
+	"syscall"
 	"xhgblog/models"
 	"xhgblog/routers"
 	"xhgblog/utils/setting"
@@ -13,5 +15,22 @@ func main() {
 
 	r := routers.InitRouter()
 
-	r.Run(fmt.Sprintf(":%d", setting.AppSetting.HttpPort))
+	readTimeout := setting.AppSetting.Server.ReadTimeout
+	writeTimeout := setting.AppSetting.Server.WriteTimeout
+	endPoint := fmt.Sprintf(":%d", setting.AppSetting.Server.HttpPort)
+	maxHeaderBytes := 1 << 20
+
+	endless.DefaultReadTimeOut = readTimeout
+	endless.DefaultWriteTimeOut = writeTimeout
+	endless.DefaultMaxHeaderBytes = maxHeaderBytes
+	server := endless.NewServer(endPoint, r)
+	server.BeforeBegin = func(add string) {
+		fmt.Printf("Actual pid is %d\n", syscall.Getpid())
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Server err: %v\n", err)
+	}
+	//r.Run(fmt.Sprintf(":%d", setting.AppSetting.HttpPort))
 }
